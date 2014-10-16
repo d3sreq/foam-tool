2d29a079867d0fd1d98008736650e9441e33c399
jiri-vinarek <jiri.vinarek@gmail.com>
2014-10-16 15:36:49 +0200

    Added servlet error handling

--- org.foam.orion.server.proof/src/org/foam/orion/server/proof/FoamServlet.java
+++ org.foam.orion.server.proof/src/org/foam/orion/server/proof/FoamServlet.java
@@ -6,49 +6,60 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.orion.server.core.OrionConfiguration;
+import org.eclipse.orion.server.core.ServerStatus;
+import org.eclipse.orion.server.core.metastore.ProjectInfo;
 import org.eclipse.orion.server.servlets.OrionServlet;
+import org.eclipse.osgi.util.NLS;
 
-@SuppressWarnings("serial")
 public class FoamServlet extends OrionServlet {
-	
+
+	private static final long serialVersionUID = 5416626682163508012L;
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
-			String pathInfo = req.getPathInfo();
-			IPath path = pathInfo == null ? Path.ROOT : new Path(pathInfo);
-//			
-			System.out.println(path);
+			String pathInfo = request.getPathInfo();
+			IPath path = pathInfo == null ? Path.EMPTY : new Path(pathInfo);
 			
-//			IFileStore file = NewFileServlet.getFileStore(req, path);
-//			
-//			// skipping symlink testing
-//			
-//			IFileInfo fileInfo = file.fetchInfo(EFS.NONE, null);
-////			file.delete(options, monitor);
-//			
-//			String dir = fileInfo.toString();
-//			System.out.println(dir);
-//			
-////			ProcessBuilder builder = new ProcessBuilder("ls", "-l", );
-////			builder.redirectErrorStream(true);
-////			Process process = builder.start();
-////
-////			InputStreamReader reader = new InputStreamReader(process.getInputStream());
-////			String result = CharStreams.toString(reader);							
-////			process.destroy();
-//			
-			String result = "TODO";
+			if (path.segmentCount() < 3) {
+				String message = NLS.bind("Path must have at least 3 segments : {0}", pathInfo);
+				ServerStatus status = new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_FORBIDDEN, message, null);
+				handleException(response, status);
+				return;
+			}
 			
-			response.getWriter().write("Response from FoamServlet class:\n" + result);
-			///
-//		} catch (IOException e) {
-//			// should not occur
-//			throw new RuntimeException(e);
-		} catch (Exception e) {
+			if (!"file".equals(path.segment(0))) {
+				String message = NLS.bind("Path must start with \"file\": {0}", pathInfo);
+				ServerStatus status = new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_FORBIDDEN, message, null);
+				handleException(response, status);
+				return;
+			}
+			
+			// expected path format is 'file/{workspaceId}/{projectName}/{path}]'
+			// for example /file/jiri-OrionContent/CoCoME
+			String workspaceId = path.segment(1);
+			String projectName = path.segment(2);
+			
+			ProjectInfo project = OrionConfiguration.getMetaStore().readProject(workspaceId, projectName);
+			if (project == null) {
+				String message = NLS.bind("Project not found: {0}", pathInfo);
+				ServerStatus status = new ServerStatus(IStatus.ERROR, HttpServletResponse.SC_NOT_FOUND, message, null);
+				handleException(response, status);
+				return;
+			}
+			
+			String absolutePath = project.getProjectStore().toLocalFile(EFS.NONE, null).getAbsolutePath();
+			response.getWriter().write("Response from FoamServlet class:\n" + absolutePath);
+		} catch (CoreException e) {
 			throw new RuntimeException(e);
 		}
 	}
+	
 }
